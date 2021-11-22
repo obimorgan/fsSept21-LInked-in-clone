054902640883a805cd6e751baa4ddecef146658b
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-22 23:01:02 +0100

    added api.

---
+++ linkedin-app/src/ApiCalls/index.js
@@ -0,0 +1,15 @@
+export const fetchProfiles = async () => {
+  let response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/profile/",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorisation:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTliYjQxN2QzNzU4MDAwMTU0OWI5ZmQiLCJpYXQiOjE2Mzc1OTQxMzUsImV4cCI6MTYzODgwMzczNX0.GF65NsMQUDXPIszIMXhd7gBE5ThkNe1QjyfkYnFy8Tg",
+      },
+    }
+  );
+
+  let Profiles = await response.json();
+  return Profiles;
+};
--- linkedin-app/src/Components/Sidebar/MyProfileSidebar.jsx
+++ linkedin-app/src/Components/Sidebar/MyProfileSidebar.jsx
@@ -1,19 +1,34 @@
-import { Nav, Button, Col, Image} from 'react-bootstrap'
+
+import { Nav, Button, Col, Image } from 'react-bootstrap'
+import { fetchProfiles } from '../../ApiCalls'
+import { useState, useEffect } from 'react'
 
 const MyProfilesSidebar = () => {
+
+    const [profiles, setProfiles] = useState()
+
+    useEffect(() => {
+        fetchProfiles(setProfiles).then((res) => setProfiles(res))
+    }, [])
+
     return (
         <Nav className="flex-column MyProfilesSidebarItem border rounded mt-3">
             <h5 className="h5ProfileHeader">People you may know</h5>
-            <div className="d-flex">
-                <Col md={2} className="ml-auto my-auto px-0">
-                <Image src="holder.js/171x180" roundedCircle />
-                </Col>
-                <Col md={10} className="flex-column">
-                    <div className="nav-link px-0 py-0 pName">Anders Hjerthen</div>
-                    <div className="nav-link px-0 py-0 pJob">Politibetjent</div>
-                    <Button variant="text-" className="btn3 border-dark rounded-pill mt-2">Connect</Button>
-                </Col>
-            </div>
+            {
+                profiles && profiles((profile) =>
+                    <div className="d-flex">
+                        <Col md={2} className="ml-auto my-auto px-0">
+                            <Image src={profile.image} roundedCircle />
+                        </Col>
+                        <Col md={10} className="flex-column">
+                            <Image src={profile.image} roundedCircle />
+                            <div className="nav-link px-0 py-0 pName">{profile.name}{profile.surname}</div>
+                            <div className="nav-link px-0 py-0 pJob">{profile.title}</div>
+                            <Button variant="text-" className="btn3 border-dark rounded-pill mt-2">Connect</Button>
+                        </Col>
+                    </div>
+                )
+            }
         </Nav>
     )
 }
