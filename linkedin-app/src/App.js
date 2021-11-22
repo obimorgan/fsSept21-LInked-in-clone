8643ebc7eb792e27b4057df472dda9faa42724c6
alimukhtor <james1999007@gmail.com>
2021-11-22 16:59:44 +0100

    random

--- linkedin-app/src/App.js
+++ linkedin-app/src/App.js
@@ -2,17 +2,12 @@ import './App.css';
 import MyJumbotron from './components/MyJumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <BrowserRouter>
     <Container>
 
      <MyJumbotron />
     </Container>
-      <Routes>
-      </Routes>
-    </BrowserRouter>
   );
 }
 
