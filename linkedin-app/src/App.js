e4f2b57890ddb0d6a8b6480d2f4b679b2a4cec0c
alimukhtor <james1999007@gmail.com>
2021-11-22 16:16:16 +0100

    added contact info for the user

--- linkedin-app/src/App.js
+++ linkedin-app/src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import MyJumbotron from './components/MyJumbotron';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Container} from 'react-bootstrap'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 function App() {
   return (
+    <BrowserRouter>
     <Container>
+
      <MyJumbotron />
     </Container>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
