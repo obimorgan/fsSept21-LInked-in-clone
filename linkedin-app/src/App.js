7f43821b4518393173d7c711a6732c738ef2ec34
alimukhtor <james1999007@gmail.com>
2021-11-22 11:35:01 +0100

    removed cols and rows

--- linkedin-app/src/App.js
+++ linkedin-app/src/App.js
@@ -4,14 +4,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 function App() {
   return (
     <Container fluid>
-      <Row className="px-0">
-        <Col md={9} className="px-0">
-        </Col>
-        <Col md={2}>
-          <Col>
-          </Col>
-        </Col>
-      </Row>
+     
     </Container>
   );
 }
