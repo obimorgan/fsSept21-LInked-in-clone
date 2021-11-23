e121b2b70ca39e7e46af9d60bce96a8d0dbc4fc5
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-23 10:45:43 +0100

    added gitignore for the authorization token

--- linkedin-app/src/ApiCalls/index.js
+++ linkedin-app/src/ApiCalls/index.js
@@ -4,9 +4,8 @@ export const fetchProfiles = async () => {
     {
       headers: {
         "Content-Type": "application/json",
-        Authorization:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTliYjQxN2QzNzU4MDAwMTU0OWI5ZmQiLCJpYXQiOjE2Mzc1OTQxMzUsImV4cCI6MTYzODgwMzczNX0.GF65NsMQUDXPIszIMXhd7gBE5ThkNe1QjyfkYnFy8Tg",
-      },
+        Authorization:`Bearer ${process.env.REACT_APP_JWT_TOKEN}`
+     },
     }
   );
 
