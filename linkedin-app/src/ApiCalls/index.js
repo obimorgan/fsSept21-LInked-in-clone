c7aaf187992da4c7425db4c6c967e0903b76ae94
Roby Morgan <roby.c.morgan@gmail.com>
2021-11-23 19:30:34 +0100

    ..

--- linkedin-app/src/ApiCalls/index.js
+++ linkedin-app/src/ApiCalls/index.js
@@ -12,9 +12,15 @@ export const fetchProfiles = async () => {
   let allUserProfiles = await response.json();
   return allUserProfiles;
 };
+<<<<<<< Updated upstream
 export const fetchPersonalProfileInfo = async () => {
   let response = await fetch(
     "https://striveschool-api.herokuapp.com/api/profile/me",
+=======
+export const fetchPersonalProfile = async (id) => {
+  let response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/profile/" + id,
+>>>>>>> Stashed changes
     {
       headers: {
         "Content-Type": "application/json",
@@ -25,5 +31,9 @@ export const fetchPersonalProfileInfo = async () => {
 
   let personalProfile = await response.json();
   return personalProfile;
+<<<<<<< Updated upstream
 };
 
+=======
+};
+>>>>>>> Stashed changes
