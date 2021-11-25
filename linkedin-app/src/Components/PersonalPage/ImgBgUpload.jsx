7ea50cfea702e55dfc22d172eb2490f64d621f8c
PiotrWachowiakpl <piotrwachowiak81@gmail.com>
2021-11-25 12:52:52 +0100

    Update ImgBgUpload.jsx

--- linkedin-app/src/Components/PersonalPage/ImgBgUpload.jsx
+++ linkedin-app/src/Components/PersonalPage/ImgBgUpload.jsx
@@ -1,9 +1,9 @@
 import { Jumbotron, Button } from "react-bootstrap"
 
 const ImgBgUpload =()=>(
-<Jumbotron className="bg-light mt-2 p-1 text-dark">
+<Jumbotron className="bg-light mt-2 p-1 text-dark border">
 <div className="d-flex justify-content-between">
-  <h5>Profile photo</h5> 
+  <h5>Background photo</h5> 
   <Button className="mx-5" variant="light"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg></Button>
@@ -12,7 +12,7 @@ const ImgBgUpload =()=>(
 <img  className="img-fluid" src="assets/cover2.jpg" alt=""  />
 </div>
 
-<div className="d-flex justify-content-end mb-5">
+<div className="d-flex justify-content-end m-2">
 <Button variant="light"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-crop" viewBox="0 0 16 16">
   <path d="M3.5.5A.5.5 0 0 1 4 1v13h13a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2H3.5a.5.5 0 0 1-.5-.5V4H1a.5.5 0 0 1 0-1h2V1a.5.5 0 0 1 .5-.5zm2.5 3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4H6.5a.5.5 0 0 1-.5-.5z"/>
 </svg><br/>Crop</Button>
@@ -26,7 +26,7 @@ const ImgBgUpload =()=>(
 </svg><br/>Frames</Button>
 
 </div>
-<div className="d-flex justify-content-center ">
+<div className="d-flex justify-content-center mt-5 mb-4">
 <Button variant="light border mx-4"> Delete photo</Button>
 
 <Button variant="light rounded-pill border mx-4"> Change Photo</Button>
